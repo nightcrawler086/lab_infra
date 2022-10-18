d6e76b6b6928dbf6ace58eed348d0673d907fdde
nightcrawler086 <brian.hall4@gmail.com>
2022-10-17 23:24:52 -0400

    a new endpoint and a ingress service that works

--- auth/src/app.controller.ts
+++ auth/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api/users/currentuser')
+  getUsers(): string {
+    return this.appService.getUsers();
+  }
 }
--- auth/src/app.service.ts
+++ auth/src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello from minikube!';
   }
+  getUsers(): string {
+    return 'The users should be here';
+  }
 }
