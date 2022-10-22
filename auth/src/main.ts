2e31b66312a43ebf02b45226196bfba9a73fe0ba
nightcrawler086 <brian.hall4@gmail.com>
2022-10-21 23:54:29 -0400

    trying to add swagger

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API Doc')
+    .setDescription('swagger for the auth microservice')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
