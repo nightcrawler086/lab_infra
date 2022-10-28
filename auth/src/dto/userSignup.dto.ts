6f3fa4b008b3fbb9fa7d2389251cb4375e24f332
nightcrawler086 <brian.hall4@gmail.com>
2022-10-27 22:38:21 -0400

    adding some data validation

--- auth/src/dto/userSignup.dto.ts
+++ auth/src/dto/userSignup.dto.ts
@@ -1,6 +1,11 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 // Expected data object for a user signup
 class userSignupDTO {
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @Length(8, 16)
     password: string;
 }
 
--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,10 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Swagger??
   const config = new DocumentBuilder()
     .setTitle('Auth API Doc')
     .setDescription('swagger for the auth microservice')
@@ -14,6 +15,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Input validation
+  app.useGlobalPipes(new ValidationPipe())
+
+  // Launch app
   await app.listen(3000);
 }
 bootstrap();
