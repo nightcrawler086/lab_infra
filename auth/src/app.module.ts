fdc6fe188f1d3507e1ad3d2e3c84c5704de9c982
nightcrawler086 <brian.hall4@gmail.com>
2022-11-13 21:50:56 -0500

    adding in mongodb deployment/service to the app.

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRootAsync({
+    useFactory: async () => ({
+      uri: 'mongodb://auth-mongo-srv:27017/auth',
+    }),
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
