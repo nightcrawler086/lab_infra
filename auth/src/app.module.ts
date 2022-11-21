1d59687f7856bd30840e823d6ed942f31fdefab4
nightcrawler086 <brian.hall4@gmail.com>
2022-11-20 21:47:44 -0500

    installing some packages. switching to typeorm for the db connection

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,14 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity'
+//import { MongooseModule } from '@nestjs/mongoose';
+//import { mongo } from 'mongoose';
 
 @Module({
-  imports: [MongooseModule.forRootAsync({
-    useFactory: async () => ({
-      uri: 'mongodb://auth-mongo-srv:27017/auth',
-    }),
-  })],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: 'auth-mongo-srv',
+    port: 27017,
+    database: 'auth',
+    entities: [User],
+    synchronize: true,
+    useUnifiedTopology: true
+  })
+    // Using one for testing and one for our users
+    //MongooseModule.forRootAsync({
+    //  useFactory: async () => ({
+    //    uri: 'mongodb://auth-mongo-srv:27017/test',
+    //    connectionName: 'test'
+    //  }),
+    //}),
+    // Async??
+    //MongooseModule.forRootAsync({
+    //  useFactory: async () => ({
+    //    uri: 'mongodb://auth-mongo-srv:27017/users',
+    //    connectionName: 'users'
+    //  }),
+    //})
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
